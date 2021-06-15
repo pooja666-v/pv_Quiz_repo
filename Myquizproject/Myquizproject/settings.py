39505e5a62e74bf2e56102f7605d8d03c2f1e3cd
Pooja Vasawade <58145453+pooja666-v@users.noreply.github.com>
2021-06-15 18:19:21 +0530

    Update settings.py

--- Myquizproject/Myquizproject/settings.py
+++ Myquizproject/Myquizproject/settings.py
@@ -49,9 +49,10 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    #'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -128,9 +129,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS=[
-STATIC_DIR,
- ]
+STATICFILES_DIRS=[STATIC_DIR,]
+STATIC_ROOT= os.path.join(BASE_DIR,'static')
+
 
 LOGIN_REDIRECT_URL='/afterlogin'
 
